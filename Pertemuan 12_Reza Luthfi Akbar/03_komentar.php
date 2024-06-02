d80ad06480bc7f4076a585a3575de26ffd64c840
reza <102231043+rezaluthfi@users.noreply.github.com>
2024-06-02 21:20:41 +0700

    add pertemuan 12

---
+++ Pertemuan 12_Reza Luthfi Akbar/03_komentar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komentar PHP</title>
+</head>
+<body>
+    <?php 
+    // Ini adalah komentar dalam satu baris
+
+    # Ini juga komentar dalam satu baris
+    
+    /*
+        Ini adalah komentar dalam banyak baris
+        Yang baru
+    */
+
+    $x = 5 /* + 15 */ + 5;
+    echo $x;
+    ?>
+</body>
+</html>
